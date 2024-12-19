21f2a799d9d262408159afb4b67361f89c1333f5
Maker34034 <mihailscorpion339@gmail.com>
2024-12-19 19:12:34 +0300

    {Hello JS}

---
+++ functional.js
@@ -0,0 +1,21 @@
+const button_1 = document.getElementById('buttonVHS');
+
+// Обработчик события нажатия на кнопку
+button_1.addEventListener('click', function() {
+    window.location.href = 'Web tape.html'; 
+});
+
+const button_2 = document.getElementById('buttonART');
+button_2.addEventListener('click', function() {
+    window.location.href = 'Art.html'; 
+});
+
+const button_3 = document.getElementById('buttonCoo');
+button_3.addEventListener('click', function() {
+    window.location.href = 'Cooperation.html'; 
+});
+
+const button = document.getElementById('buttonGoHome');
+button.addEventListener('click', function() {
+    window.location.href = 'Main.html'; 
+});
